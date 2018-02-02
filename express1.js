10f980eafec4f88cb3e90e73ad40ca94d506a22e
13451883267 <1131328108@qq.com>
2018-02-02 15:17:47 +0800

    用express做了一个简单的接口

--- express1.js
+++ express1.js
@@ -6,13 +6,34 @@
  *    get  post  use(通吃)
  */
 const express = require('express');
+const expressStatic = require('express-static');
 
 var server = express();
-server.use('/', (request, response) => {
-  // send 比 write 是 express 对 write 的改进版
-  // 原来的 write 不能读 json，现在的 send 就可以
-  response.send('<h1>test</h1>');
-  response.end();
-});
+// server.use('/', (request, response) => {
+//   // send 比 write 是 express 对 write 的改进版
+//   // 原来的 write 不能读 json，现在的 send 就可以
+//   response.send('<h1>test</h1>');
+//   response.end();
+// });
+
+const user = {
+  test: 123
+}
+
+server.get('/login', (req, res) => {
+  let userInfo = req.query
+  let name = userInfo.name
+  let pass = userInfo.pass
+  if(user[name] == null) {
+    res.send({ok: false, msg: '用户不存在'})
+  } else if (user[name] != pass) {
+    res.send({ok: false, msg: '用户名或密码不正确'})
+  } else {
+    res.send({ok: true, msg: '登录成功'})
+  }
+  res.end()
+})
+// 处理用户对文件的访问
+server.use(expressStatic('./www'));
 
 server.listen(4040);
