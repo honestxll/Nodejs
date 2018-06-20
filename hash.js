d49ebf7293f8b3eada221b0b99a141129a65df2d
honest <1131328108@qq.com>
2018-06-20 22:46:59 +0800

    init basic template

--- hash.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-const hash = crypto.createHash('sha256');
-hash.update('woshics6102acgniw');
-// console.log(hash.digest('hex'));
-console.log(hash.digest('base64'));
--- md5.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-var cryptoObj = crypto.createHash('md5');
-
-cryptoObj.update('123456');
-
-var result = cryptoObj.digest('hex');
-
-console.log(result);
