9e2936e65b13018b10a5269daa6d6a57d6d72866
honest <1131328108@qq.com>
2018-06-20 23:10:48 +0800

    基本的文件上传，注意要添加enctype=multipart/form-data

---
+++ index.js
@@ -0,0 +1,30 @@
+const multer = require('multer')
+const express = require('express')
+const upload = multer({ dest: 'uploads/' })
+const app = express()
+
+app.post('/profile', upload.single('avatar'), (req, res, next) => {
+  res.send(req.file)
+})
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head><meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    </head>
+    <body>
+      <form action="http://localhost:8080/profile" enctype="multipart/form-data" method="post">
+        <input type="file" name="avatar" />
+        <button type="submit">上传</button>
+      </form>
+    </body>
+    </html>
+  `)
+})
+app.listen(8080, () => {
+  console.log('http://localhost:8080')
+})
