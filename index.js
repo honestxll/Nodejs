20eac5d53bdaabd5724e2866ef046187a0dd162e
honest <1131328108@qq.com>
2018-06-20 23:16:53 +0800

    添加多文件上传

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.post('/profile', upload.single('avatar'), (req, res, next) => {
   res.send(req.file)
 })
 
+app.post('/photos', upload.array('photos'), (req, res, next) => {
+  res.send(req.files)
+})
+
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -19,6 +23,25 @@ app.get('/', (req, res) => {
     <body>
       <form action="http://localhost:8080/profile" enctype="multipart/form-data" method="post">
         <input type="file" name="avatar" />
+        <button type="submit">上传</button>
+      </form>
+    </body>
+    </html>
+  `)
+})
+
+app.get('/multiple', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head><meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    </head>
+    <body>
+      <form action="http://localhost:8080/photos" enctype="multipart/form-data" method="post">
+        <input type="file" name="photos" multiple/>
         <button type="submit">上传</button>
       </form>
     </body>
